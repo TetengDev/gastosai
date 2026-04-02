61668d0003227549f605b4c7fbefcf2f0673b690
LesterBryanIlao <lestertetengilao@gmail.com>
2026-04-02 08:14:08 +0800

    dev: initial commit

---
+++ gastosai/src/main/java/com/teng/app/gastosai/GastosaiApplication.java
@@ -0,0 +1,13 @@
+package com.teng.app.gastosai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastosaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastosaiApplication.class, args);
+	}
+
+}
